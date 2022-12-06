5cd3a2e4fa205a1ed14b1f2cf17e546fe0538559
PleBea <bini060315kim@gmail.com>
2022-12-07 00:06:01 +0900

    chore: Setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
+import '../styles/_globals.scss'
 import type { AppProps } from 'next/app'
+import Header from '../components/Header'
+import Meta from '../components/Meta'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Meta />
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
